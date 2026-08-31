2b8c0d4edc988129757cb6494ac1f0b0fbd6d08d
osafoopare299-cmd <osafoopare299@gmail.com>
2026-08-31 11:35:49 -0700

    Add lecturer education workspace route

---
+++ student-results-portal/app/education/lecturer/page.js
@@ -0,0 +1,5 @@
+import Workspace from '../workspace';
+
+export const metadata = { title: 'Lecturer Portal | Dropare Education' };
+
+export default function LecturerPortal(){ return <Workspace role="lecturer"/>; }
